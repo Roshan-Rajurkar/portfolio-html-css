005dbede2a898d2104a4113430d1c8623156717a
Roshan-Rajurkar <roshangit50@gmail.com>
2023-02-01 06:18:26 +0530

    Profile Site

---
+++ About.js
@@ -0,0 +1,4 @@
+let url = document.getElementById('about');
+url.addEventListener('click',function(){
+    location.href = 'about.html';
+})
---
+++ contact.js
@@ -0,0 +1,8 @@
+// var popup = document.querySelector(".popup");
+
+// function openPopup() {
+//   popup.classList.add("open-popup");
+// }
+// function closePopup() {
+//   popup.classList.remove("open-popup");
+// }
